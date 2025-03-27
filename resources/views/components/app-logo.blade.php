8a8fd1c7ef2fb85463c01f744fd32e05b81c648f
shivangi023-png <78527207+shivangi023-png@users.noreply.github.com>
2025-03-27 17:44:15 +0530

    authenication with sanctum and create society form

---
+++ resources/views/components/app-logo.blade.php
@@ -0,0 +1,9 @@
+{{-- <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+</div> --}}
+<div class="flex aspect-square size-9 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+    <img src="{{ asset('images/logo.png') }}" alt="" class="w-full h-full">
+</div>
+<div class="ml-1 grid flex-1 text-left text-sm">
+    <span class="mb-0.5 truncate leading-none font-semibold">Share Certificate</span>
+</div>
